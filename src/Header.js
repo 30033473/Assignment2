1377847652eeea73bd43cf00a8e32b43ed7799f2
30033473 <30033473@student.toiohomai.ac.nz>
2021-06-11 23:09:03 +1200

    june update

--- src/Header.js
+++ src/Header.js
@@ -1,4 +1,5 @@
 import React,{Component} from "react";
+import headervideo from "./images/header_vid.webm"
 
 class Header extends Component
 {
@@ -8,12 +9,10 @@ class Header extends Component
             <div>
             <section class="py-5 text-center container">
             <div class="row py-lg-5">
-            <div class="col-lg-6 col-md-8 mx-auto shadow-lg bg-dark w-100">
-              <h1 class="text-warning"><strong >SCP Foundation</strong></h1>
+            <div class="col-lg-6 col-md-8 mx-auto  bg-dark w-100">
+                <video autoPlay loop muted className="mx-auto w-100">
+                <source src={headervideo} type="video/webm" /></video>
               <hr class="text-warning" />
-              <p class="text-danger"><strong>WARNING: THE FOUNDATION DATABASE IS</strong></p>
-              <h2 class="text-danger"><strong>CLASSIFIED</strong></h2>
-              <p class="text-danger"><strong>ACCESS BY UNAUTHORIZED PERSONNEL IS STRICTLY PROHIBITED PERPETRATORS WILL BE TRACKED, LOCATED, AND DETAINED</strong></p>
             </div>
             </div>
             </section>
--- src/SCP.js
+++ src/SCP.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import barrierImage from "./images/barrier.png"
 
 const Scp = ({scpinfo}) => {
     return(<div>
@@ -9,14 +10,20 @@ const Scp = ({scpinfo}) => {
                         <div className="container">
                             <div className="row row-cols-sm-2 cols-md-3 g-3"></div>
                                 <div className="card shadow-lg bg-dark">
-                                    <img className="mx-auto d-block w-50" src={scp.image} />
+                                    <img className="mx-auto d-block w-50" alt="scp" src={scp.image} />
                                     <div className="col-md-12">
                                         <div className="card-body text-warning">
                                             <h1 className="card-title text-center">{scp.item}</h1>
+                                            <h3 className="card-subtitle mb-2 text-center">CLASS: {scp.class}</h3>
                                             <hr />
-                                            <h3 className="card-subtitle mb-2 text-muted">{scp.class}</h3>
-                                            <p className="card-text">{scp.containment}</p>
+                                            <br></br>
+                                            <h3 className="card-subtitle mb-2 text-muted">DESCRIPTION :</h3>
                                             <p className="card-text">{scp.description}</p>
+                                            <br></br>
+                                            <h3 className="card-subtitle mb-2 text-muted">CONTAINMENT :</h3>
+                                            <p className="card-text">{scp.containment}</p>  
+                                            <br></br>                                            
+                                            <img className="mx-auto d-block w-100 h-50" alt="scp" src={barrierImage} />                                        
                                         </div>
                                     </div>
                                 </div>

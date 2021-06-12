aa2e3ad37b862b664c9e2d5a646cd01c530132f9
30033473 <70986813+30033473@users.noreply.github.com>
2021-06-12 15:25:46 +1200

    Update Header.js

--- src/Header.js
+++ src/Header.js
@@ -9,10 +9,9 @@ class Header extends Component
             <div>
             <section class="py-1 text-center container">
             <div class="row py-lg-1">
-            <div class="col-lg-6 col-md-8 mx-auto  bg-dark w-100">
+            <div class="col-lg-6 col-md-8 mx-auto  bg-dark w-100 shadow">
                 <video autoPlay loop muted className="mx-auto w-100">
                 <source src={headervideo} type="video/webm" /></video>
-              <hr class="text-warning" />
             </div>
             </div>
             </section>
